7e54f497290ed32484e6f7f51ea95c15f0fe78a7
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-09-20 19:06:12 -0700

    Uppercase string printed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-     string = ""
+    string = ""
     for i in range(len(str)):
         if ord(str[i]) < ord('z') + 1 and ord(str[i]) > ord('a') - 1:
             string += chr(ord(str[i]) - 32)
