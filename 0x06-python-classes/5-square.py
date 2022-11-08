97488ca14bb17f0dcda48de78b5e5148f7640203
Lois Onyeanuforo <loisonyeanuforo@gmail.com>
2022-11-08 00:02:14 -0800

    Python classes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+""" A class square."""
+
+
 class Square():
-    """A square class."""
+    """Representation of a class square."""
+
     def __init__(self, size=0):
-        """Initialization of the class square."""
+        """Initialization of the class square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
 
     def area(self):
